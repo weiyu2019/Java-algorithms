72db5af66ab56900a0a00f7bfe7bc9e9fa8aa3f0
weiyu2019 <weiyu1913@163.com>
2021-01-24 18:02:33 +0800

    first commit

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -14,10 +14,15 @@ public class Fibonacci {
     // Methods all returning [0, 1, 1, 2, 3, 5, ...] for n = [0, 1, 2, 3, 4, 5, ...]
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-
+    long time1 = System.currentTimeMillis();
     System.out.println(fibMemo(n));
+    System.out.println(System.currentTimeMillis() - time1);
+    long time2 = System.currentTimeMillis();
     System.out.println(fibBotUp(n));
+    System.out.println(System.currentTimeMillis() - time2);
+    long time3 = System.currentTimeMillis();
     System.out.println(fibOptimized(n));
+    System.out.println(System.currentTimeMillis() - time3);
     sc.close();
   }
 
